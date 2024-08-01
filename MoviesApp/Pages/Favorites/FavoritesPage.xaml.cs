cfc8744fa609b0eba8e05719e60febaf539b7bbf
JosePreatorianWise <jose.david.bustos.h@gmail.com>
2024-07-31 21:08:42 -0400

    primer commit

---
+++ MoviesApp/Pages/Favorites/FavoritesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MoviesApp
+{
+    public partial class FavoritesPage : ContentPage
+    {
+        public FavoritesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
